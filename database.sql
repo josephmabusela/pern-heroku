bc8a539b497c25b554b24a58b1ca95d3a87752a9
josephmabusela <josephmabusela5@gmail.com>
2022-06-06 18:38:21 +0200

    initial commit

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE perntodo;
+
+CREATE TABLE todo(
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
+
+INSERT INTO todo (description) VALUES("Build more React Webapps");
