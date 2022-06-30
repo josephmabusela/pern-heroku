d0c1a127934eee21ab05559db110771b300fa458
josephmabusela <josephmabusela5@gmail.com>
2022-06-30 12:24:56 +0200

    homepage config

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const pool = require("./db");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-// preoces.env.PORT
+// precess.env.PORT
 // process.env.NODE_ENV => production or undefined
 
 //middleware
 app.use(cors());
 app.use(express.json()); // => allows access to req.body
 
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"))
 
 if(process.env.NODE_ENV === "production") {
   // server static content
