cdb711e9c5a7a09577a85886f0a9dc76e1beabbe
josephmabusela <josephmabusela5@gmail.com>
2022-07-07 15:40:55 +0200

    ready for deployment

---
+++ client/build/precache-manifest.7670858ca0eed3fcb53af6f0e3954f8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d60aad4f2284e5aa4524",
+    "url": "/pern-heroku/static/css/main.4a26f06c.chunk.css"
+  },
+  {
+    "revision": "d60aad4f2284e5aa4524",
+    "url": "/pern-heroku/static/js/main.e7b34138.chunk.js"
+  },
+  {
+    "revision": "d3662c55c36270ca8fbf",
+    "url": "/pern-heroku/static/js/runtime~main.fcfa6590.js"
+  },
+  {
+    "revision": "fec025540c32172d7d79",
+    "url": "/pern-heroku/static/js/2.3c7b1ff5.chunk.js"
+  },
+  {
+    "revision": "015e1d531543fa79418fad6ab1a93007",
+    "url": "/pern-heroku/index.html"
+  }
+];
