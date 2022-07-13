f54a4b227775dec020319564dded996a99f1fd50
josephmabusela <josephmabusela5@gmail.com>
2022-07-13 14:00:40 +0200

    test

--- db.js
+++ db.js
@@ -29,6 +29,10 @@ const proConfig = process.env.DATABASE_URL; //heroku addons
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
 });
 
 module.exports = pool;
